4b2335c9e50f3c3454307d5630b29899e9e3721a
Marvin Arnold <marvinmarnold@gmail.com>
2015-06-19 15:26:56 -0500

    Added PGP keys

--- client/templates/research/research.js
+++ client/templates/research/research.js
@@ -8,3 +8,12 @@ Deps.autorun(function () {
     var map = L.mapbox.map('map', 'mapbox.streets').setView([29.94228045, -90.07880318], 14);
   }
 });
+
+Template.research.rendered = function () {
+  this.autorun(function () {
+    if (Mapbox.loaded()) {
+      L.mapbox.accessToken = Meteor.settings.public.mapboxPublicToken;
+      var map = L.mapbox.map('map', 'mapbox.streets').setView([29.94228045, -90.07880318], 14);
+    }
+  });
+};
