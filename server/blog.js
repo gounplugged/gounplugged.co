7f643dcfab590a1c5f736163b2973d0685027feb
Marvin Arnold <marvinmarnold@gmail.com>
2015-06-19 16:49:48 -0500

    Blog added

---
+++ server/blog.js
@@ -0,0 +1,7 @@
+Blog.config({
+  rss: {
+    title: 'Unplugged Blog',
+    description: 'Thoughts.'
+  },
+  blogNotFoundTemplate: 'notFound'
+});
---
+++ server/fixtures.js
@@ -0,0 +1,11 @@
+if ( Meteor.users.find().count() === 0 ) {
+    Accounts.createUser({
+        email: 'marvin@gounplugged.co',
+        password: Meteor.settings.marvinPassword,
+        profile: {
+            first_name: 'Marvin',
+            last_name: 'Arnold',
+            company: 'Unplugged'
+        }
+    });
+}
