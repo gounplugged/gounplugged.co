fa66bbebe088dda0ff955134a101ba023863aed0
Marvin Arnold <marvinmarnold@gmail.com>
2015-06-19 15:12:02 -0500

    Added mapbox

---
+++ client/templates/research/research.js
@@ -0,0 +1,10 @@
+Meteor.startup(function(){
+    Mapbox.load('minimap', 'markercluster');
+});
+
+Deps.autorun(function () {
+  if (Mapbox.loaded()) {
+    L.mapbox.accessToken = Meteor.settings.public.mapboxPublicToken;
+    var map = L.mapbox.map('map', 'mapbox.streets').setView([29.94228045, -90.07880318], 14);
+  }
+});
