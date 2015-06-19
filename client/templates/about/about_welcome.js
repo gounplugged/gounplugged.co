aafb641b35a83cc053592cd57cbb58774f090be1
Marvin Arnold <marvinmarnold@gmail.com>
2015-06-19 10:34:29 -0500

    signup form moved to template

--- client/templates/about/about_welcome.js
+++ client/templates/about/about_welcome.js
@@ -1,24 +1,33 @@
 Template.aboutWelcome.events({
-  'click #signup-button': function(e) {
-    e.preventDefault();
-    var email = $('#signup-email').first();
-    var address = email.val();
-
-    if(!address) return;
-
-    var signup = {
-      email: address,
-    };
-
-    email.val('');
-    Meteor.call('signupCreate', signup, function(error, padId) {
-      if (error){
-        notify("Sorry, " + address + " has already registered.", "warning");
-      } else {
-        notify("Thank you, " + address + " has been registered.", "success");
-      }
-
-      return;
-    });
+  'click .signup-button': function(e) {
+    signup(e);
+  },
+  'keypress .signup-email': function(e) {
+    if (e.which === 13) {
+      return signup(e);
+    }
+    return;
   },
 });
+
+var signup = function(e) {
+  e.preventDefault();
+  var email = $('.signup-email').first();
+  var address = email.val();
+
+  if(!address) return;
+
+  var signup = {
+    email: address,
+  };
+
+  email.val('');
+  Meteor.call('signupCreate', signup, function(error, padId) {
+    if (error){
+      notify("Sorry, " + address + " has already registered.", "warning");
+    } else {
+      notify("Thank you, " + address + " has been registered.", "success");
+    }
+    return;
+  });
+};
